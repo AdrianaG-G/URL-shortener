9b01c20a1b154a4e14e10d0bb2a2493d03a56616
Adriana <adrianamgonzalezg@gmail.com>
2019-03-05 18:27:49 -0600

    Simple View
    Simple view to help tests

--- app/controllers/url_shorteners_controller.rb
+++ app/controllers/url_shorteners_controller.rb
@@ -1,15 +1,19 @@
 class UrlShortenersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
   end
 
   def create
     @url = UrlShortener.new
-    puts "Enter the URL you want to shorten"
-    @url.original_url = gets.chomp
+    @url.original_url = params[:original_url]
     if @url.save
       puts "New URL generated correctly."
+      render 'index'
     else
       puts "Sorry, we had problems generating the URL. Try again."
+      render 'index'
     end
   end
+
 end
