f3d2e593ae9a8306af98fd1b981c78efe4200854
Adriana <adrianamgonzalezg@gmail.com>
2019-03-06 22:07:13 -0600

    Route list top 100

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get "/show_short_url/:short_url", to: "url_shorteners#show_short_url", as: :show_short_url
   get "/url_shorteners/get_original_url"
   get "/:short_url", to: "url_shorteners#get_original_url"
+  get "url_shorteners/list_top_100"
 end
 
