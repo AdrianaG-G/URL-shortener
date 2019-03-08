cc4e17419774a65880c139f964a28029695e0df1
Adriana <adrianamgonzalezg@gmail.com>
2019-03-07 22:36:49 -0600

    Edit Readme

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'url_shorteners#index'
-  get 'url_shorteners/index'
-  get 'url_shortener/index'
 
   post "url_shorteners/create"
   get "/show_short_url/:short_url", to: "url_shorteners#show_short_url", as: :show_short_url
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_06_065801) do
+ActiveRecord::Schema.define(version: 2019_03_06_185331) do
 
   create_table "url_shorteners", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "short_url"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_03_06_065801) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.integer "visits", default: 0
   end
 
 end
