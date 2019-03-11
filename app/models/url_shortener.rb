f3c0095c09ae92e53f0ddc60c7d73737cd21567e
Adriana <adrianamgonzalezg@gmail.com>
2019-03-11 01:30:39 -0600

    New Shortener URL algorithm
    Shortening URLs using Base 62 Encoding

--- app/models/url_shortener.rb
+++ app/models/url_shortener.rb
@@ -1,29 +1,31 @@
+# frozen_string_literal: true
+
 class UrlShortener < ApplicationRecord
-    before_create :generate_new_short_url
-    validates :original_url, presence: true
+  validates :original_url, presence: true
 
-    SHORTEST_LENGTH = 7
-    require 'securerandom'
-
-    def generate_new_short_url
-        #produces random alphanumeric strings
-        new_url = SecureRandom.alphanumeric(SHORTEST_LENGTH) 
-
-        #Check short_url doesn't exist in the database.
-        url_exist = UrlShortener.find_by_short_url(new_url)
-        if url_exist.nil?
-          self.short_url = new_url
-        else
-          #If short_url exists, we generate a new url.
-          self.generate_new_short_url
-        end    
+  # Generates short_url for id specified using Base62 encoding
+  def generate_short_url(url_id)
+    # Base62 alphabet to be used
+    alphabet_array = [*'a'..'z', *'A'..'Z', *'0'..'9']
+    shorturl = []
+    while url_id.to_i.positive?
+      shorturl.push(alphabet_array[url_id % 62])
+      url_id /= 62
     end
-    
-    def find_duplicate
-        UrlShortener.find_by_original_url(self.original_url)
-    end
+    new_url = shorturl.reverse.join
 
-    def url_not_saved?
-      find_duplicate.nil?
-    end
+    # Check short_url doesn't exist in the database. If short_url exists, we generate a new url
+    url_exist = UrlShortener.find_by_short_url(new_url)
+    return new_url if url_exist.blank?
+
+    generate_short_url(url_id)
+  end
+
+  def find_duplicate
+    UrlShortener.find_by_original_url(original_url)
+  end
+
+  def url_not_saved?
+    find_duplicate.blank?
+  end
 end
