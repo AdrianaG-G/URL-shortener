ad8ed5ce13afe6a980b403c45bdab13991ae8ee1
Adriana <adrianamgonzalezg@gmail.com>
2019-03-11 01:30:18 -0600

    Rubocop

--- app/controllers/url_shorteners_controller.rb
+++ app/controllers/url_shorteners_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class UrlShortenersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
@@ -9,31 +11,32 @@ class UrlShortenersController < ApplicationController
   def create
     @url = UrlShortener.new
     @url.original_url = params[:original_url]
-
     if @url.url_not_saved?
       if @url.save
         GetTitleJob.perform_later @url.id
-        flash[:success] = "URL shortened correctly."
+        add_short_url(@url.id)
+
+        flash[:success] = 'URL shortened correctly.'
+        @url = UrlShortener.find(@url.id)
         redirect_to show_short_url_path(@url.short_url)
       else
-        flash.now[:error] = "Sorry, the URL could not be generated."
+        flash.now[:error] = 'Sorry, the URL could not be generated.'
         render 'index'
       end
     else
-      get_original_url = UrlShortener.find_by_original_url(params[:original_url])
       redirect_to show_short_url_path(@url.find_duplicate.short_url)
     end
   end
 
-  def get_original_url
+  def find_original_url
     url = UrlShortener.find_by_short_url(params[:short_url])
-    #Increment it on each page view
+    # Increment it on each page view
     url.increment!(:visits)
     redirect_to url.original_url
   end
 
   def show_short_url
-    #Returns a \host:\port string for this request
+    # Returns a \host:\port string for this request
     host = request.host_with_port
     @url = UrlShortener.find_by_short_url(params[:short_url])
     @original_url = @url.original_url
@@ -42,5 +45,11 @@ class UrlShortenersController < ApplicationController
 
   def list_top_100
     @urls = UrlShortener.order('visits DESC').limit(100)
+  end
+
+  def add_short_url(url_id)
+    url = UrlShortener.find(url_id)
+    url.short_url = url.generate_short_url(url_id)
+    url.save
   end
 end
